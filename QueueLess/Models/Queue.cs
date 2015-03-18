bc0b07b57626ebf4967e414c58e76b66df0645e2
stephania07 <stephaniaiyob@yahoo.com>
2015-03-17 19:29:31 -0500

    initial commit

---
+++ QueueLess/Models/Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QueueLess.Models
+{
+    public class Queue
+    {
+        public int ID { get; set; }
+        public string Service { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string RegistrationTime { get; set; }
+        public string CurrentTime { get; set; }
+        public int EstimatedMinutes { get; set; }
+        public int PeopleOnQueue { get; set; }
+
+
+        public Queue()
+        {
+
+        }
+        public Queue(string Service, string FirstName, string LastName, string PhoneNumber, string Email, string RegistrationTime, string CurrentTime, int EstimatedMinutes, int PeopleOnQueue)
+        {
+            this.Service = Service;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
+            this.RegistrationTime = RegistrationTime;
+            this.CurrentTime = CurrentTime;
+            this.EstimatedMinutes = EstimatedMinutes;
+            this.PeopleOnQueue = PeopleOnQueue;
+
+        }  
+    }
+}
