bc7683fbd0bd11c219c4d9fcd0216b979bad9100
stephania07 <stephaniaiyob@yahoo.com>
2015-03-18 08:43:29 -0500

    Added QAPIControllers & Views WIP

--- QueueLess/App_Start/WebApiConfig.cs
+++ QueueLess/App_Start/WebApiConfig.cs
@@ -14,6 +14,7 @@ namespace QueueLess
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            //http://www.asp.net/web-api/overview/getting-started-with-aspnet-web-api/build-a-single-page-application-%28spa%29-with-aspnet-web-api-and-angularjs
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- QueueLess/Models/Queue.cs
+++ QueueLess/Models/Queue.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+
 
 namespace QueueLess.Models
 {
     public class Queue
     {
         public int ID { get; set; }
+
+        [Required]
         public string Service { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Must be 10 digits")]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
         public string Email { get; set; }
         public string RegistrationTime { get; set; }
         public string CurrentTime { get; set; }
