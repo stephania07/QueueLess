49254870ba2d2ab986f7b5b6c66ac3546b73fc5f
stephania07 <stephaniaiyob@yahoo.com>
2015-03-22 16:25:49 -0500

    counts the total # of queus ahead

--- QueueLess/Scripts/app/config.js
+++ QueueLess/Scripts/app/config.js
@@ -2,21 +2,44 @@
     .config(function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'Static/View/Home.html' });
         $routeProvider.when('/JoinQueue',{templateUrl: 'Static/View/JQueue.html', controller: 'AddQueueController'});
-        $routeProvider.when('/ShowQueue',{templateUrl: 'Static/View/SQueue.html', controller: 'ShowQueueController'});
+        $routeProvider.when('/ShowQueue', { templateUrl: 'Static/View/SQueue.html', controller: 'ShowQueueController' });
+
     })
 
 .controller('AddQueueController', function($scope, queueRepository, $location){
     $scope.queues = queueRepository.query();
 
     $scope.save = function () {
-       // console.log($scope.queue);
         queueRepository.save($scope.queue);
         $location.url("/ShowQueue");
     }
 })
-.controller('ShowQueueController', function ($scope, queueRepository) {
+.controller('ShowQueueController', function ($scope, queueRepository, $location) {
+    $scope.queues = queueRepository.query();
+})
+
+    //To be revised
+.controller('ShowQueueController', function ($scope, queueRepository, $location) {
     $scope.queues = queueRepository.query();
 
+    $scope.delete = function () {
+        queueRepository.delete($scope.queue);
+        $location.url("/JoinQueue");
+    }
 })
+
+        //counts the queue
+.controller('queueCountCtrl', function ($scope, queueRepository) {
+        $scope.queuecount = queueRepository.query();
+})
+
+    //counts by category/service = to be revised
+.controller('serviceCountCtrl', function ($scope, queueRepository) {
+    $scope.queueservice = queueRepository.query();
+    })
+
+
+
+
 
 
